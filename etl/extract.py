3982bbf9e91840751bd53579a3bb188a8a8ba4b0
TiagoFernandes404 <tiagojosefernandes2103@gmail.com>
2026-06-08 12:49:44 +0100

    feat: complete extract functions for all endpoints

--- etl/extract.py
+++ etl/extract.py
@@ -18,3 +18,22 @@ def fetch_teams(competition_id):
     URL = f"{BASE_URL}competitions/{competition_id}/teams"
     response = requests.get(URL, headers=headers)
     return response.json()
+
+def fetch_topscorers(competition_id):
+    URL = f"{BASE_URL}competitions/{competition_id}/scorers"
+    response = requests.get(URL, headers=headers)
+    return response.json()
+
+def fetch_standings(competition_id):
+    URL = f"{BASE_URL}competitions/{competition_id}/standings"
+    response = requests.get(URL, headers=headers)
+    return response.json()
+
+def fetch_matches(competition_id):
+    URL = f"{BASE_URL}competitions/{competition_id}/matches"
+    response = requests.get(URL, headers=headers)
+    return response.json()
+
+
+# we go and get all the things from the api some dont need to have a unique like a person because they are include in
+# the other ones, like persons is in teams
